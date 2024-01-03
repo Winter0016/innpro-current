d8cb42efc3d9bb985ede1fcf05ef67072576ab4b
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-01-03 19:25:34 +0700

    product page

--- src/pages/Product/product.jsx
+++ src/pages/Product/product.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PRODUCTS } from '../../constants';
+import { ShopContext } from '../../context/shopContext';
+import {images } from '../../constants/images';
+import { Product_item } from './product-item';
+import "./product.css"
 
 export const Product = () => {
+
+  const {xnumber} = useContext(ShopContext);
+  console.log(`xnumb is ${xnumber}`);
   return (
-    <div>Product</div>
+    <div className='inspect-container'>
+      <div className='myproduct-container'>
+        {PRODUCTS.map((product) => {
+          if(product.id === xnumber) {
+            return <Product_item data={product} />
+          }
+        })} 
+      </div>
+    </div>
   )
 }
--- src/pages/Shop/container/Show/product.jsx
+++ src/pages/Shop/container/Show/product.jsx
@@ -1,15 +1,22 @@
+// Product.jsx
 import React, { useContext } from 'react';
 import { ShopContext } from '../../../../context/shopContext';
 import { images } from '../../../../constants';
+import { useNavigate } from 'react-router-dom';
 
 export const Product = (props) => {
-  const {id, productName, price, productImage} = props.data;
-  const {addToCart} = useContext(ShopContext);
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, inspect} = useContext(ShopContext);
+  const navigate = useNavigate();
 
+  const handleClick = () => {
+    inspect(id);
+    navigate('/product');
+  };
 
   return (
     <div className='product'>
-      <img className='product-image' src={productImage} alt='a' />
+      <img className='product-image' onClick={handleClick} src={productImage}  alt='a' />
       <div className='description'>
         <div className="product-name"> {productName} </div>
         <div className='product-price'> {price} vnđ </div>
@@ -18,6 +25,5 @@ export const Product = (props) => {
         <img className='order' src={images.orderButton} alt='a' />
       </button>
     </div>
-  )
-}
-
+  );
+};
